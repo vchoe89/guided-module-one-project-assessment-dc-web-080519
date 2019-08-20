6bd67954762672fcf31fa2a58ec909b2d49e856e
github username <github email address>
2019-08-20 12:22:11 -0400

    scratch

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ pc = System.find_or_create_by(name:"PC")
 ps4 = System.find_or_create_by(name:"PlayStation 4")
 xb1 = System.find_or_create_by(name:"Xbox One")
 swi = System.find_or_create_by(name:"Nintendo Switch")
-7e79310d3b327fcb8cc3253ee3f57b0d
+
 #Availableons
 # Availableon.find_or_create_by_by(system_id: pc.id, game_id: pubg.id)
 # Availableon.find_or_create_by(system_id: pc.id, game_id: fortnite)
@@ -49,7 +49,7 @@ swi = System.find_or_create_by(name:"Nintendo Switch")
 # Availableon.find_or_create_by(system_id: swi.id, game_id: fortnite)
 # Availableon.find_or_create_by(system_id: pc.id, game_id: bttlefld1942)
 
-<<<<<<< HEAD
+
 # binding.pry
 
 pc.games = [pubg, bttlefld1942, apex, red_dead, cod, star_wars, sea_of_thieves, minecraft, snipperclips, anthem, cyberpunk, stardew_valley, overcooked2, rkt_league, sc2, csgo]
@@ -59,4 +59,4 @@ swi.games = [fortnite, zelda, mario_kart, super_mario, tetri, snipperclips, star
 
 binding.pry
 =======
-#binding.pr3ee3f57b0d
+
